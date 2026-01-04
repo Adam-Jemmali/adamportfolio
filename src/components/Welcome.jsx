cd49958951640189079cad5416e0d1adf53fcb3a
Adam Jemmali <trapdimej@gmail.com>
2026-01-03 19:06:58 -0500

    Welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,30 +1,94 @@
-import React, {useRef} from 'react'
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const renderText = (text, className, baseWeight = 400) => {
+    return [...text].map((char, i) => (
+        <span
+            key={i}
+            className={ className}
+            style={{ fontVariationSettings: `"wght" ${baseWeight}` }}
+        >
+      {char === " " ? "\u00A0" : char}
+    </span>
+    ));
+};
+
+const FONTS_WEIGHTS = {
+    subtitle: { min: 100, max: 400, default: 100 },
+    title: { min: 400, max: 900, default: 400 },
+};
+
+// hover selection logic
+const setTextHover = (container, type) => {
+    if (!container) return;
+
+    const letters = container.querySelectorAll("span");
+    const { min, max,default: base} = FONTS_WEIGHTS[type];
+
+    const animateLetter = (letter, weight, duration = 0.25) => {
+        return gsap.to(letter, {
+            duration,
+            ease: "power4.out",
+            fontVariationSettings: `"wght" ${weight}`,
+        });
+    };
+
+    const mousemove = (e) => {
+        const { left } = container.getBoundingClientRect();
+        const mouseX = e.clientX - left;
+
+        letters.forEach((letter) => {
+            const { left: l, width: w } = letter.getBoundingClientRect();
 
 
-    const renderText= (text,className,baseWeight=400) => {
-    return [text].map((char,i) => (
-        <span key= {i}  className={className}
-            style= {{fontVariationSettings: `'whgt ${baseWeight}`}} >
-            {char ===     " " ? "\u00A0" : char}
-        </span>
-        ))
+            const distance = Math.abs(mouseX - (l - left + w / 2));
+            const intensity = Math.exp(-(distance ** 2) / 20000);
+
+            animateLetter(letter, min+(max-min)*intensity);
+        });
+    };
+    const mouseleave= () =>
+        letters.forEach((letter) =>  animateLetter(letter,base,0.3))
 
 
+    container.addEventListener("mousemove", mousemove);
+    container.addEventListener("mouseleave", mouseleave);
 
+    return() => {
+        container.removeEventListener("mousemove", mousemove);
+        container.removeEventListener("mouseleave", mouseleave);
     }
 
+};
 
-    const Welcome = () => {
-    const titleRef= useRef(null);
+const Welcome = () => {
+    const titleRef = useRef(null);
     const subtitleRef = useRef(null);
-    return (
-        <section id= "welcome">
-            <p ref={subtitleRef}>
-                {renderText("Im adam welcome to my portfolio","text-3xl font-georama",100)}</p>
-            <h1 ref={titleRef}   className="mt-7">
-                PORTFOLIO</h1>
 
+    useGSAP(() => {
+        const cleanup= setTextHover(titleRef.current, "title");
+        const subtitleCleanup = setTextHover(subtitleRef.current, "subtitle");
+
+        return() => {
+            cleanup();
+            subtitleCleanup ();
+        }
+
+
+    }, []);
+
+    return (
+        <section id="welcome">
+            <p ref={subtitleRef}>
+                {renderText("Im Adam! Welcome to my ", "text-1xl font-georama", 100)}
+            </p>
+
+            <h1 ref={titleRef} className="mt-7">
+                {renderText("PORTFOLIO", "text-6xl italic font-georama",400)}
+            </h1>
         </section>
-    )
-}
-export default Welcome
+    );
+};
+
+export default Welcome;
